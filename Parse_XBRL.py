c5434983086fc8f26be5a5217e8602fd5baaab40
Guang Yih Sheu <xsheu@hotmail.com>
2019-07-09 21:33:36 +0800

    2019 7/9 21:33

--- Parse_XBRL.py
+++ Parse_XBRL.py
@@ -3,12 +3,7 @@ Created on Jul 9, 2019
 
 @author: xsheu
 '''
-import urllib
 import math
-import matplotlib.pyplot as plt
-import warnings
-import numpy as np
-
 
 def xbrl_parse(xbrl_str):
     xbrl_file=open(xbrl_str)
@@ -231,20 +226,32 @@ def KolmogorovSmirnov_conclude(value,total):
         return -1.0
     else:
         return 1.0
+def ydetermine(ymin,ymax,value):
+    if(value<=ymin):
+        ymin=value
+    if(value>=ymax):
+        ymax=value
+    return ymin,ymax    
+    
         
 file_str = "/Users/xsheu/eclipse-workspace/parse_XBRL/filelist.txt"
-xbrl_str=""
+output_file="/Users/xsheu/Desktop/xbrl2018.txt"
 total=0
 f=open(file_str)
+fo=open(output_file)
 for component in f:
     digital = [0.0 for i in range(99)]
     actual = [0.0 for i in range(99)]
+    ymax1d=0.0
+    ymin1d=0.0
+    ymax2d=0.0
+    ymin2d=0.0
     theoretical_digital = [0.0 for i in range(99)]
     theoretical_value = [0.0 for i in range(99)]
     for j in range(1,99):
         value=theoretical(1,j,0.0,0.0)
         theoretical_digital[j]=value
-    total,digital = xbrl_parse(xbrl_str)
+    total,digital = xbrl_parse(component)
     opinion=1
     level=0.1
     c=0.0
@@ -256,15 +263,27 @@ for component in f:
     kuiper1d=Kuiper(1,actual,theoretical_value)
     KolmogorovSmirnov1d=KolmogorovSmirnov(1,actual,theoretical_value)
     mad1dconclude=mad_conclude(1,mad1d)
+    ymin1d,ymax1d=ydetermine(ymin1d,ymax1d,mad1dconclude)
     chi1dconclude=chi_conclude(1,level,chi1d)
+    ymin1d,ymax1d=ydetermine(ymin1d,ymax1d,chi1dconclude)
     kuiper1dconclude=Kuiper_conclude(kuiper1d,total)
-    KolmogorovSmirnov1d=KolmogorovSmirnov_conclude(KolmogorovSmirnov1d,total)
+    ymin1d,ymax1d=ydetermine(ymin1d,ymax1d,kuiper1dconclude)
+    KolmogorovSmirnov1dconclude=KolmogorovSmirnov_conclude(KolmogorovSmirnov1d,total)
+    ymin1d,ymax1d=ydetermine(ymin1d,ymax1d,KolmogorovSmirnov1dconclude)
     KolmogorovSmirnov2d=0
     mad2d=mad(2,1,digital,c,n)
     chi2d=chi(2,1,digital,c,n,total)
     kuiper2d=Kuiper(2,actual,theoretical_value)
     KolmogorovSmirnov2d=KolmogorovSmirnov(2,actual,theoretical_value)
     mad2dconclude=mad_conclude(2,mad2d)
+    ymin2d,ymax2d=ydetermine(ymin2d,ymax2d,mad2dconclude)
     chi2dconclude=chi_conclude(2,level,chi2d)
+    ymin2d,ymax2d=ydetermine(ymin2d,ymax2d,chi2dconclude)
     kuiper2dconclude=Kuiper_conclude(kuiper2d,total)
-    KolmogorovSmirnov2d=KolmogorovSmirnov_conclude(KolmogorovSmirnov2d,total)
+    ymin2d,ymax2d=ydetermine(ymin2d,ymax2d,kuiper2dconclude)
+    KolmogorovSmirnov2dconclude=KolmogorovSmirnov_conclude(KolmogorovSmirnov2d,total)
+    KolmogorovSmirnov2dconclude=KolmogorovSmirnov_conclude(KolmogorovSmirnov2d,total)
+    output_string = component[32,:]+" "+str(ymin1d)+" "+str(ymax1d)+" "+str(ymin2d)+" "+str(ymax2d)
+    fo.write(output_string)
+f.close()
+fo.close()    
